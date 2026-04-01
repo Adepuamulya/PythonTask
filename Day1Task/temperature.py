da8efaa0bd5006b2df2f81cde0103eb3aca82ad3
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:19:04 +0530

    Update temperature.py

--- Day1Task/temperature.py
+++ Day1Task/temperature.py
@@ -1,6 +1,4 @@
 #Ask the user for a temperature in Celsius and print it
-# Ask the user for temperature in Celsius
 celsius = float(input("Enter temperature in Celsius: "))
 
-# Print the temperature
 print("The temperature in Celsius is:", celsius)
