4a9c403c58f0fb903a231cd31e0f7db315c0d692
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:28:00 +0530

    Update continue.py

--- Day2Task/continue.py
+++ Day2Task/continue.py
@@ -1,6 +1,5 @@
 #Write a program using continue to skip printing the number 3 in a loop from 1 to 10.
-# Loop from 1 to 10
 for num in range(1, 11):
     if num == 3:
-        continue  # Skip the rest of the loop for num = 3
+        continue 
     print(num, end=" ")
