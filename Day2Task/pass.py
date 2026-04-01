817e4053bba8d6e4eb733fcb85bcc47cb7fdc3b1
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:32:22 +0530

    Update pass.py

--- Day2Task/pass.py
+++ Day2Task/pass.py
@@ -1,6 +1,5 @@
 #Write a program that uses pass inside a loop
-# Loop from 1 to 5
 for num in range(1, 6):
     if num == 3:
-        pass  # Do nothing when num is 3
+        pass 
     print(num, end=" ")
