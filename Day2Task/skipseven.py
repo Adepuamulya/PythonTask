c79a40457b4538dcd60520b9128314570da5a8d5
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:33:56 +0530

    Update skipseven.py

--- Day2Task/skipseven.py
+++ Day2Task/skipseven.py
@@ -1,6 +1,5 @@
 #Write a program that prints numbers from 1 to 10 but skips even numbers using continue
-# Loop from 1 to 10
 for num in range(1, 11):
     if num % 2 == 0:
-        continue  # Skip even numbers
+        continue
     print(num, end=" ")
