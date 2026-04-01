1ca01ffec042afbe2015e8512a3426ce92edbbd0
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:14:51 +0530

    Update square.py

--- Day1Task/square.py
+++ Day1Task/square.py
@@ -1,9 +1,4 @@
 #Ask the user for a number and print its square
-# Take input from the user
 num = float(input("Enter a number: "))
-
-# Calculate square
 square = num * num
-
-# Print the result
 print("The square of the number is:", square)
