bb7c78a722bb906ace9ab112fbf71c7ce297dab1
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 12:54:36 +0530

    Update assignment.py

--- Day1Task/assignment.py
+++ Day1Task/assignment.py
@@ -1,10 +1,6 @@
 # Demonstrate the use of assignment operators (`+=`, `-=`)
 num = 10
-
-# Using += operator
-num += 5   # same as num = num + 5
+num += 5 
 print("After += :", num)
-
-# Using -= operator
-num -= 3   # same as num = num - 3
+num -= 3 
 print("After -= :", num)
