2e7d3c1dfa9297b5f36f994ded39c1ac9df988c3
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 12:52:42 +0530

    Update aligntext.py

--- Day1Task/aligntext.py
+++ Day1Task/aligntext.py
@@ -1,12 +1,8 @@
 #Align text using formatting
-name = "Alice"
-age = 25
-
-# Left-align, width 10
+name = "Ammu"
+age = 23
 print(f"Name: {name:<10} Age: {age}")
 
-# Right-align, width 10
 print(f"Name: {name:>10} Age: {age}")
 
-# Center-align, width 10
 print(f"Name: {name:^10} Age: {age}")
