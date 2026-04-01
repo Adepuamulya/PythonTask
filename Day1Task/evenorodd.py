eb142df31ef2aaa1f767b72daab17de91f9cb306
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 12:59:47 +0530

    Update evenorodd.py

--- Day1Task/evenorodd.py
+++ Day1Task/evenorodd.py
@@ -1,8 +1,6 @@
 #Use modulus operator to check if a number is even or odd
-# Taking input from user
 num = int(input("Enter a number: "))
 
-# Checking even or odd
 if num % 2 == 0:
     print("The number is Even")
 else:
