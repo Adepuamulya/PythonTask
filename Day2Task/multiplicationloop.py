49cfc07fef7f861ede530efb09e340dd490c9d39
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:31:57 +0530

    Update multiplicationloop.py

--- Day2Task/multiplicationloop.py
+++ Day2Task/multiplicationloop.py
@@ -1,8 +1,5 @@
 #Write a program to print the multiplication table of a number using a loop.
-# Ask the user to enter a number
 num = int(input("Enter a number to print its multiplication table: "))
-
-# Print the multiplication table using a for loop
 print(f"Multiplication table of {num}:")
 for i in range(1, 11):
     print(f"{num} x {i} = {num * i}")
