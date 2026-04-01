f9f3578b9e2595af5b6a201d6a6902462e2b8be2
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:29:47 +0530

    Update factorialloop.py

--- Day2Task/factorialloop.py
+++ Day2Task/factorialloop.py
@@ -1,16 +1,9 @@
 #Write a program to calculate the factorial of a number using a loop.
-# Ask the user to enter a number
 num = int(input("Enter a non-negative integer: "))
-
-# Initialize factorial to 1
 factorial = 1
-
-# Check for negative input
 if num < 0:
     print("Factorial is not defined for negative numbers.")
 else:
-    # Calculate factorial using a for loop
     for i in range(1, num + 1):
         factorial *= i
-    # Display the result
     print(f"The factorial of {num} is: {factorial}")
