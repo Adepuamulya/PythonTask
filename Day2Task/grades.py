6a4eea5efbf227f13a501ca27f7aa78241edbacc
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:30:35 +0530

    Update grades.py

--- Day2Task/grades.py
+++ Day2Task/grades.py
@@ -1,8 +1,5 @@
 #Write a program to assign grades based on marks (for example: A, B, C, Fail). 
-# Ask the user to enter marks
 marks = float(input("Enter your marks (0-100): "))
-
-# Assign grades based on marks
 if marks >= 90 and marks <= 100:
     grade = 'A'
 elif marks >= 75:
@@ -13,6 +10,4 @@ elif marks >= 0:
     grade = 'Fail'
 else:
     grade = 'Invalid marks'
-
-# Display the grade
 print(f"Your grade is: {grade}")
