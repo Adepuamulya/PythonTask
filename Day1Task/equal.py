e50718449f09091fdbeb8dfc499d33dc0d9486f3
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 12:59:25 +0530

    Update equal.py

--- Day1Task/equal.py
+++ Day1Task/equal.py
@@ -1,9 +1,7 @@
 #Write a program that checks if two numbers are equal
-# Taking input from user
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-# Checking equality
 if num1 == num2:
     print("Both numbers are equal")
 else:
