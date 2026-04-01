76f40982131073493cd41c8b03caec6c39abbd65
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:41:58 +0530

    Update students-marks.py

--- Day3Task/students-marks.py
+++ Day3Task/students-marks.py
@@ -1,8 +1,8 @@
 #Create a dictionary with 3 student names and their marks
 students = {
-    "Alice": 85,
-    "Bob": 90,
-    "Charlie": 78
+    "Ammu": 85,
+    "Nani": 90,
+    "Chintu": 78
 }
 
 print("Student marks:", students)
