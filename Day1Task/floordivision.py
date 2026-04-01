92e22644b60a34e9eb9a3e20dd328652623b3ac7
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:00:51 +0530

    Update floordivision.py

--- Day1Task/floordivision.py
+++ Day1Task/floordivision.py
@@ -1,10 +1,7 @@
 #Use floor division operator to divide two numbers
-# Taking input from user
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-# Floor division
 result = num1 // num2
 
-# Display result
 print("Result (Floor Division):", result)
