86edde31caf4538d218703a4a02ea856108e7b3c
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 12:55:22 +0530

    Update avgnumb.py

--- Day1Task/avgnumb.py
+++ Day1Task/avgnumb.py
@@ -1,11 +1,8 @@
 #Write a program that takes three numbers as input and prints their average. 
-# Take three numbers as input
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 num3 = float(input("Enter third number: "))
 
-# Calculate the average
 average = (num1 + num2 + num3) / 3
 
-# Print the average
 print("The average is:", average)
