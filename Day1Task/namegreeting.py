3d6632c714c88904794586298d6b05a51eab12ab
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:05:19 +0530

    Update namegreeting.py

--- Day1Task/namegreeting.py
+++ Day1Task/namegreeting.py
@@ -1,6 +1,3 @@
 #Write a Python program that asks the user for their name and prints a greeting message
-#  Ask the user for their name
 name = input("Enter your name: ")
-
-# Print a greeting message
 print("Hello, " + name + "! Welcome!")
