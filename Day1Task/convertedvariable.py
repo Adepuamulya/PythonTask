f38b3d5b76f6f1a63a47975aacb8054fd2ff954b
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 12:57:42 +0530

    Update convertedvariable.py

--- Day1Task/convertedvariable.py
+++ Day1Task/convertedvariable.py
@@ -1,9 +1,6 @@
 #Write a program to display the type of converted variable
 num_str = "100"
-
-# Convert string to integer
 num_int = int(num_str)
 
-# Display value and its type
 print(num_int)
 print(type(num_int))
