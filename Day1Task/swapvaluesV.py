2d4ef3a083cc84d159cbdc48bee09f4930c774a4
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:16:35 +0530

    Update swapvaluesV.py

--- Day1Task/swapvaluesV.py
+++ Day1Task/swapvaluesV.py
@@ -1,13 +1,10 @@
 #Swap the values of two variables
-# Initialize variables
 a = 5
 b = 10
 
-# Swap using a temporary variable
 temp = a
 a = b
 b = temp
 
-# Print swapped values
 print("a =", a)
 print("b =", b)
