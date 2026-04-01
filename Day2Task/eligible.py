9eec7a40534d6d94ec9dd3c995137e08ed2005ce
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:28:22 +0530

    Update eligible.py

--- Day2Task/eligible.py
+++ Day2Task/eligible.py
@@ -1,8 +1,5 @@
 #Write a program to check if a person is eligible to vote (age ≥ 18)
-# Ask the user to enter their age
 age = int(input("Enter your age: "))
-
-# Check voting eligibility
 if age >= 18:
     print("You are eligible to vote.")
 else:
