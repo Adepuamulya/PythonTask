66d1968afd057a20106fd97d6f3f2c7fb8a59666
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:43:31 +0530

    Update valuesofdictionary.py

--- Day3Task/valuesofdictionary.py
+++ Day3Task/valuesofdictionary.py
@@ -1,8 +1,8 @@
 # # Write a program to print all values of a dictionary
 students = {
-    "Alice": 85,
-    "Bob": 90,
-    "Charlie": 78
+    "Ammu": 85,
+    "Nani": 90,
+    "Chintu": 78
 }
 
 for value in students.values():
