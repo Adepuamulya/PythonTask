76f307eceaf1e170f83f94d8233bd85fa2fa87d2
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:32:52 +0530

    Update positivenegativezero.py

--- Day2Task/positivenegativezero.py
+++ Day2Task/positivenegativezero.py
@@ -1,8 +1,5 @@
 #Write a program to check whether a number is positive, negative, or zero
-# Ask the user to enter a number
 num = float(input("Enter a number: "))
-
-# Check if the number is positive, negative, or zero
 if num > 0:
     print(f"{num} is positive.")
 elif num < 0:
