9ec32ce8be1b01f27233b80d2e3cbc8f351c3d25
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:40:26 +0530

    Update newkeyvalue.py

--- Day3Task/newkeyvalue.py
+++ Day3Task/newkeyvalue.py
@@ -1,10 +1,10 @@
 #Write a program to add a new key-value pair to a dictionary
 students = {
-    "Alice": 85,
-    "Bob": 90,
-    "Charlie": 78
+    "Ammu": 85,
+    "Nani": 90,
+    "Chintu": 78
 }
 
-students["David"] = 88
+students["Dolly"] = 88
 
 print("Updated dictionary:", students)
