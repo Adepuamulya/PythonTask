0d15d7e857252164a5b8e7fd67a0a2fcea029f17
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:05:42 +0530

    Update numbproduct.py

--- Day1Task/numbproduct.py
+++ Day1Task/numbproduct.py
@@ -1,10 +1,6 @@
 #Write a program that takes two numbers and prints their product
-# Take two numbers as input
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-# Calculate product
 product = num1 * num2
-
-# Print the result
 print("The product is:", product)
