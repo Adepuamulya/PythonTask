694f9a40b5874ab35b44451d6bee2e50012968ea
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-01 13:04:38 +0530

    Update lwrectangleV.py

--- Day1Task/lwrectangleV.py
+++ Day1Task/lwrectangleV.py
@@ -1,10 +1,5 @@
 # Create variables for length and width and calculate the area of a rectangle
-# Create variables
 length = 10
 width = 5
-
-# Calculate area
 area = length * width
-
-# Print the result
 print("The area of the rectangle is:", area)
