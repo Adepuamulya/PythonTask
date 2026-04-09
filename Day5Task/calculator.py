f13a1e247ab00d34d916d970bd6642f1fc295e7f
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:03:45 +0530

    Update calculator.py

--- Day5Task/calculator.py
+++ Day5Task/calculator.py
@@ -6,12 +6,9 @@
 # Then write another Python program that imports this module and performs calculations based on user input.
 def add(a, b):
     return a + b
-
 def subtract(a, b):
     return a - b
-
 def multiply(a, b):
     return a * b
-
 def divide(a, b):
     return a / b
