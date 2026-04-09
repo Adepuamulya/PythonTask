54eaa9f024db700fb5f6106276e33e4bdca77417
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:14:03 +0530

    Update word-counter.py

--- Day8Task/word-counter.py
+++ Day8Task/word-counter.py
@@ -4,17 +4,12 @@
 #● Counts the number of words, lines, and characters in the file 
 #● Displays the results.
 file = open("article.txt", "r")
-
 content = file.read()
-
 words = len(content.split())
 characters = len(content)
-
 file.seek(0)
 lines = len(file.readlines())
-
 file.close()
-
 print("Number of words:", words)
 print("Number of lines:", lines)
 print("Number of characters:", characters) 
