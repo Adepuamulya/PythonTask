59ab8baa74fec4104476de91a9348df897b3e5b0
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:44:17 +0530

    Update existsintuple.py

--- Day3Task/existsintuple.py
+++ Day3Task/existsintuple.py
@@ -1,8 +1,6 @@
 #Write a program to check whether an element exists in a tuple
 my_tuple = (10, 20, 30, 40, 50)
-
 element = 35
-
 if element in my_tuple:
     print("Element exists in the tuple")
 else:
