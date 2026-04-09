ca5a772b8cd5ad0a05eca3255560c0102b81536b
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:47:45 +0530

    Update maxminvaluesT.py

--- Day3Task/maxminvaluesT.py
+++ Day3Task/maxminvaluesT.py
@@ -1,8 +1,6 @@
 #Write a program to find the maximum and minimum values in a tuple
 my_tuple = (10, 25, 5, 40, 15)
-
 maximum = max(my_tuple)
 minimum = min(my_tuple)
-
 print("Maximum value:", maximum)
 print("Minimum value:", minimum)
