aaf220b09ccca39b3221c7f10f6693024c91e201
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:25:06 +0530

    Update combineV.py

--- Day1Task/combineV.py
+++ Day1Task/combineV.py
@@ -1,7 +1,5 @@
 #Create variables for first name and last name and combine them
 first_name = "Amulya"
 last_name = "Adepu"
-
 full_name = first_name + " " + last_name
-
 print("Full name:", full_name)
