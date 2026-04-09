9ba356ec6549ad4534afba48a4303491f77dc83b
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:09:17 +0530

    Update armstrongnumb.py

--- Day7Task/armstrongnumb.py
+++ Day7Task/armstrongnumb.py
@@ -8,15 +8,12 @@
 #Output: 
 #153 is an Armstrong number
 num = int(input("Enter a number: "))
-
 temp = num
 sum = 0
-
 while temp > 0:
     digit = temp % 10
     sum += digit ** 3
     temp //= 10
-
 if sum == num:
     print(num, "is an Armstrong number")
 else:
