a7db59c538036e129c7002ed3f4be7db10e758fa
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:37:02 +0530

    Update uppercase.py

--- Day1Task/uppercase.py
+++ Day1Task/uppercase.py
@@ -1,6 +1,4 @@
 #Take a sentence as input and print it in uppercase
 sentence = input("Enter a sentence: ")
-
 uppercase_sentence = sentence.upper()
-
 print("Uppercase sentence:", uppercase_sentence)
