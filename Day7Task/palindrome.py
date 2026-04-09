e68eeb33fc2d9c5f3b1cffa68a8fdd493782d8a7
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:09:37 +0530

    Update palindrome.py

--- Day7Task/palindrome.py
+++ Day7Task/palindrome.py
@@ -7,15 +7,12 @@
 #Output: 
 #121 is a Palindrome number
 num = int(input("Enter a number: "))
-
 temp = num
 rev = 0
-
 while temp > 0:
     digit = temp % 10
     rev = rev * 10 + digit
     temp = temp // 10
-
 if num == rev:
     print(num, "is a Palindrome number")
 else:
