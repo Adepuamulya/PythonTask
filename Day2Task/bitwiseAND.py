df660d0e18d4a48655893d203b98ed69b5d5711d
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:37:28 +0530

    Update bitwiseAND.py

--- Day2Task/bitwiseAND.py
+++ Day2Task/bitwiseAND.py
@@ -1,6 +1,5 @@
 #Write a Python program to perform bitwise AND (&) on two numbers entered by the user
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-
 result = num1 & num2
 print(f"The result of {num1} & {num2} is: {result}")
