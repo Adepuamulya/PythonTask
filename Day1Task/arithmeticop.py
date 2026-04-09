bbb4fbc9808a0d9b371ec2624d08c5ad2fca5b59
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:22:14 +0530

    Update arithmeticop.py

--- Day1Task/arithmeticop.py
+++ Day1Task/arithmeticop.py
@@ -1,6 +1,5 @@
 #Write a program to add two numbers using arithmetic operators
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-
 sum_result = num1 + num2
 print("Sum is:", sum_result)
