2550686508dab334bfc6bdab043327c511f5148c
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:48:12 +0530

    Update modifyelement.py

--- Day3Task/modifyelement.py
+++ Day3Task/modifyelement.py
@@ -1,8 +1,5 @@
 #Write a program to convert a tuple to a list and modify the element.
 my_tuple = (10, 20, 30, 40, 50)
-
 my_list = list(my_tuple)
-
 my_list[2] = 99
-
 print("Modified list:", my_list)
