45d89d8a1c1056ee4636e44926270c51c781e27c
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:16:03 +0530

    Update bank-account.py

--- Day9Task/bank-account.py
+++ Day9Task/bank-account.py
@@ -5,19 +5,14 @@ class BankAccount:
     def __init__(self, acc_no, balance):
         self.acc_no = acc_no
         self.balance = balance
-
     def deposit(self, amount):
         self.balance = self.balance + amount
-
     def withdraw(self, amount):
         self.balance = self.balance - amount
-
     def display(self):
         print("Account Number:", self.acc_no)
         print("Balance:", self.balance)
-
 a = BankAccount(104, 5000)
-
 a.deposit(2000)
 a.withdraw(1000)
 a.display()
