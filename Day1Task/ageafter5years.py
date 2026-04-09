96e5441ad02faa9e6d5bd3179200aa1281acc85a
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:21:05 +0530

    Update ageafter5years.py

--- Day1Task/ageafter5years.py
+++ Day1Task/ageafter5years.py
@@ -1,6 +1,4 @@
 #Write a program that asks the user for their age and prints the age after 5 years. 
 age = int(input("Enter your age: "))
-
 future_age = age + 5
-
 print("Your age after 5 years will be:", future_age)
