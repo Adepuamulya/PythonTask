4eccf48fd45fe7356fbd5a549e9530295712e21f
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:40:37 +0530

    Update bitwiseOR.py

--- Day2Task/bitwiseOR.py
+++ Day2Task/bitwiseOR.py
@@ -1,6 +1,5 @@
 #Write a program to perform bitwise OR (|) operation between two integers
 num1 = int(input("Enter the first integer: "))
-
 num2 = int(input("Enter the second integer: "))
 result = num1 | num2
 print(f"The result of {num1} | {num2} is: {result}")
