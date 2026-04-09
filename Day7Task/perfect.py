04c5dd8ed6e7d94f49fe2168547094f962bf95d8
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:10:03 +0530

    Update perfect.py

--- Day7Task/perfect.py
+++ Day7Task/perfect.py
@@ -8,13 +8,10 @@
 #Output: 
 #6 is a Perfect number
 num = int(input("Enter a number: "))
-
 sum = 0
-
 for i in range(1, num):
     if num % i == 0:
         sum += i
-
 if sum == num:
     print(num, "is a Perfect number")
 else:
