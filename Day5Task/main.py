b37de7d16c13a6793bf14bd3983400dd99e8bad5
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:05:39 +0530

    Update main.py

--- Day5Task/main.py
+++ Day5Task/main.py
@@ -1,9 +1,7 @@
 # continuation of calculator.py code
 import calculator
-
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
-
 print("Addition:", calculator.add(a, b))
 print("Subtraction:", calculator.subtract(a, b))
 print("Multiplication:", calculator.multiply(a, b))
