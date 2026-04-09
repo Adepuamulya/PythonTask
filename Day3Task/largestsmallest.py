003c8865ab53b4778f57daf9313801a03e36a899
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:46:16 +0530

    Update largestsmallest.py

--- Day3Task/largestsmallest.py
+++ Day3Task/largestsmallest.py
@@ -1,8 +1,6 @@
 #Write a program to find the largest and smallest element in a list.
 numbers = [10, 25, 5, 40, 15]
-
 largest = max(numbers)
 smallest = min(numbers)
-
 print("Largest element:", largest)
 print("Smallest element:", smallest)
