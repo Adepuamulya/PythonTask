e2e7ae17c2ec4820ebeecb88a567f4dd7e08ce0b
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:32:18 +0530

    Update power.py

--- Day1Task/power.py
+++ Day1Task/power.py
@@ -1,7 +1,5 @@
 #Calculate the power of a number using `**`
 base = int(input("Enter the base number: "))
 exponent = int(input("Enter the exponent: "))
-
 result = base ** exponent
-
 print("Result:", result)
