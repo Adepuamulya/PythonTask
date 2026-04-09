e989c8d9b51c00f532d41ed69e1feb04666a8604
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:02:03 +0530

    Update main.py

--- Day5Task/utilities.py/main.py
+++ Day5Task/utilities.py/main.py
@@ -5,10 +5,8 @@
 #Write a Python program that imports the package and uses functions from both modules.
 from utilities import math_operations
 from utilities import string_operations
-
 print("Addition:", math_operations.add(10, 5))
 print("Multiplication:", math_operations.multiply(10, 5))
-
 text = "python programming"
 print("Uppercase:", string_operations.to_uppercase(text))
 print("Character Count:", string_operations.count_characters(text))
