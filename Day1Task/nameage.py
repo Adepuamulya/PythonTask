e2fdbd2d7e43ca3e82b5f3fe3d3692fd04345c77
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:31:03 +0530

    Update nameage.py

--- Day1Task/nameage.py
+++ Day1Task/nameage.py
@@ -1,5 +1,4 @@
 #Take two inputs (name and age) and display them in a sentence
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
-
 print(f"My name is {name} and I am {age} years old.")
