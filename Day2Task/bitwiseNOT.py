2c54b288a37ffb6d6b7a7494e43c0cd4bcac0b91
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:39:51 +0530

    Update bitwiseNOT.py

--- Day2Task/bitwiseNOT.py
+++ Day2Task/bitwiseNOT.py
@@ -1,6 +1,4 @@
 #Write a Python program that takes a number and prints its bitwise NOT (~) value
 num = int(input("Enter a number: "))
-
 result = ~num
-
 print(f"The bitwise NOT of {num} is: {result}")
