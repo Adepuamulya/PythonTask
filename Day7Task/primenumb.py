cd6df087afad6d1fa27c9d082573a17f395837bc
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 15:10:25 +0530

    Update primenumb.py

--- Day7Task/primenumb.py
+++ Day7Task/primenumb.py
@@ -7,13 +7,10 @@
 #Factors = 1, 7 
 #7 is a Prime number
 num = int(input("Enter a number: "))
-
 count = 0
-
 for i in range(1, num + 1):
     if num % i == 0:
         count += 1
-
 if count == 2:
     print(num, "is a Prime number")
 else:
