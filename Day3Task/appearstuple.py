f6369ec77f3bb6a6e04737c98d070951a05afb78
Adepuamulya <119094677+Adepuamulya@users.noreply.github.com>
2026-04-09 14:42:29 +0530

    Update appearstuple.py

--- Day3Task/appearstuple.py
+++ Day3Task/appearstuple.py
@@ -1,8 +1,5 @@
 #Write a program to count how many times an element appears in a tuple.
 my_tuple = (10, 20, 30, 20, 40, 20, 50)
-
 element = 20
-
 count = my_tuple.count(element)
-
 print("Element appears", count, "times")
